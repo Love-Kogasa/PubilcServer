be2fc61bb37a74f359c0f1b9f5c9d398fb6656a2
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-11-16 23:09:58 +0800

    Update client.js

--- client/client.js
+++ client/client.js
@@ -7,20 +7,20 @@ class PublicServer {
          if( url.search !== "" ){
             for( let param of url.search.slice( 1 ).split( "&" ) ){
                let [key,value] = param.split( "=" )
-               retv[ decodeURI( key ) ] = decodeURI( value )
+               retv[ decodeURIComponent( key ) ] = decodeURIComponent( value )
             }
          }
          return retv
       })()
    }
    async request( url = "https://example.com", setting ){
-      return await fetch( this.server + "/request?host=" + encodeURI( url ), setting )
+      return await fetch( this.server + "/request?host=" + encodeURIComponent( url ), setting )
    }
    async cat( url = "https://example.com", method = "GET" ){
       return await (await this.request( url, {method} )).text()
    }
    async number( single = true, id = "test", add = "none" ){
-      var dt = (await fetch( this.server + "/number?id=" + encodeURI( id ) + "&add=" + encodeURI( add ) )).json()
+      var dt = (await fetch( this.server + "/number?id=" + encodeURIComponent( id ) + "&add=" + encodeURIComponent( add ) )).json()
       if( single ){
          return dt[ id ]
       } else {
@@ -33,16 +33,3 @@ class PublicServer {
 }
 
 if( typeof module === "object" ) module.exports = PublicServer
-/*
-var myserver = new PublicServer( "http://localhost:3030" )
-
-// request
-myserver.cat( "https://example.com" )
-  .then( console.log )
-
-// ip
-myserver.ip().then( ip => console.log( ip.ipv4 ) )
-
-// number
-myserver.number( false ).then( dt => console.log( dt.test ) )
-*/
