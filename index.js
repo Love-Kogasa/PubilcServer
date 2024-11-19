42b24abac4c32025a161d8863792d2398eae0291
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-11-19 20:30:04 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const express = require( "express" ),
   request = require( "sync-request" ),
   path = require( "path" ),
   bodyParser = require( "body-parser" ),
-  mailer = require( "nodemailer" )
+  mailer = require( "nodemailer" ),
+  dns = require( "dns" ),
+  net = require( "net" )
 
 function searchurl( u ){
    var search = {}
@@ -84,6 +86,55 @@ app.post( "/mail", function( req, res ){
         res.send( JSON.stringofy({statu: "error", error: e}) ) :
         res.send( JSON.stringofy({statu: "success", info: info}) )
    })
+})
+
+app.get( "/ipc", function( req, res ){
+   var setting = searchurl( req.url )
+   if( typeof setting.ip === "undefined" ){
+      res.send( JSON.stringify( { statu: "error", error : "ip?" } ) )
+   } else {
+      var count = parseInt( setting.count ) || 1
+      try {
+         if( Number.isNaN( count ) ) throw "Count error"
+         var [ip, port] = setting.ip.split( ":" )
+         var client = net.connect( parseInt(port), ip )
+         var data = []
+         client.on( "data", function( dt ){
+            data.push({ string: dt.toString(), buffer: dt.toJSON().data })
+            if( data.length == count ){
+               res.send( JSON.stringify( { statu: "success", data: data }))
+               try {
+                  client.destory()
+               } catch( e ) {
+                  client.end()
+               }
+            }
+         })
+      } catch(err) {
+         res.send( JSON.stringify( { statu: "error", error : err } ) )
+      }
+   }
+})
+
+app.get( "/dns", function( req, res ){
+   var domain = searchurl( req.url ).domain || "example.com"
+   if( domain.match( /(\d{0,3}\.){3}\d{0,3}/ ) ){
+      dns.reverse( domain, function( err, ipdata ){
+         if(!err){
+            res.send( JSON.stringify( {domain, data: ipdata}) )
+         } else {
+            res.send( JSON.stringify( {domain, data: err}))
+         }
+      } )
+   } else {
+      dns.resolve4( domain, function( err, ipdata ){
+         if( !err ){
+            res.send( JSON.stringify( {domain, data: ipdata}) )
+         } else {
+            res.send( JSON.stringify( {domain, data: err}))
+         }
+      } )
+   }
 })
 
 app.post( "/request", function( req, res ){
